d53649e21ebc9c770bbf25bb2a4aa6c5efd02068
jasminejeane <jasminejeane@gmail.com>
2018-04-24 22:03:42 -0700

    string var

--- Introductions.java
+++ Introductions.java
@@ -5,7 +5,8 @@ public class Introductions {
     public static void main(String[] args) {
         Console console = System.console();
         // Welcome to the Introductions program!  Your code goes below here
-        console.printf("Hello my name is Jasmine");
-        console.printf("Learning to print java");
+        String firstName = "Jazz";
+        console.printf("Hello my name is %s \n", firstName);
+        console.printf("%s is Learning to print java\n", firstName);
   }
 }
